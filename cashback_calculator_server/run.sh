13c312d77e443c8e54bc8a9416e7f3eff8b9158e
RzaIs <rzaismail82@gmail.com>
2023-11-27 00:48:32 +0400

    profile sperated & cashback urls added

--- cashback_calculator_server/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-uvicorn main:app --host localhost --port 8083
--- src/main/java/is/rza/wallet/client/CashbackClient.java
+++ src/main/java/is/rza/wallet/client/CashbackClient.java
@@ -2,16 +2,32 @@ package is.rza.wallet.client;
 
 import is.rza.wallet.model.dto.CashbackDTO;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.math.BigDecimal;
 
+@Profile("!local")
 @FeignClient(
         name = "CashbackClient",
-        url = "localhost:8083"
+        url = "${client.cashback.url}"
 )
 public interface CashbackClient {
     @GetMapping("/cashback")
     CashbackDTO calculateCashback(@RequestParam BigDecimal transactionAmount);
+}
+
+@Component
+@Profile("local")
+class CashbackClientMock implements CashbackClient {
+    @Override
+    public CashbackDTO calculateCashback(BigDecimal transactionAmount) {
+        var response = new CashbackDTO();
+        response.setCashbackAmount(
+                transactionAmount.multiply(BigDecimal.valueOf(0.1))
+        );
+        return response;
+    }
 }
